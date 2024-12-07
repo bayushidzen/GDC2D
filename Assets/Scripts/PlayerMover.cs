018f94748dd2b5da2cb2a4cc5ad09e7a9c83b2db
bayushidzen <zhek_wh@mail.ru>
2024-12-07 22:12:49 +0300

    HW2_10 Make Oval player and update speed calculating

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMover : MonoBehaviour
 {
+    private const float speedBooster = 20f;
     [SerializeField] private float _speedX = 0.5f;
     private Rigidbody2D _rigidbody;
     void Start()
@@ -13,7 +14,7 @@ public class PlayerMover : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        _rigidbody.velocity = new Vector2(_speedX, _rigidbody.velocity.y);
+        _rigidbody.velocity = new Vector2(_speedX * speedBooster * Time.fixedDeltaTime, _rigidbody.velocity.y);
         
     }
 }
