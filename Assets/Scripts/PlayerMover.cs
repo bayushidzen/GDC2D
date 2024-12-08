b3931be62192757423587b5d7774cd571dac9723
bayushidzen <zhek_wh@mail.ru>
2024-12-08 12:17:06 +0300

    HW2_11 Add movement control

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -6,15 +6,23 @@ using UnityEngine;
 public class PlayerMover : MonoBehaviour
 {
     private const float speedBooster = 20f;
+    private const string HorizontalAxis = "Horizontal";
     [SerializeField] private float _speedX = 0.5f;
     private Rigidbody2D _rigidbody;
+    private float direction;
+
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
     }
+    private void Update()
+    {
+        direction = Input.GetAxis(HorizontalAxis);
+        Debug.Log(direction);
+    }
     private void FixedUpdate()
     {
-        _rigidbody.velocity = new Vector2(_speedX * speedBooster * Time.fixedDeltaTime, _rigidbody.velocity.y);
+        _rigidbody.velocity = new Vector2(_speedX * direction * speedBooster * Time.fixedDeltaTime, _rigidbody.velocity.y);
         
     }
 }
