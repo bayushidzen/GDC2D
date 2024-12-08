b213be98270b98add07a80916a30d601ea28cc17
bayushidzen <zhek_wh@mail.ru>
2024-12-08 13:25:11 +0300

    HW2_12 Player now can jump

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMover : MonoBehaviour
 {
-    private const float speedBooster = 20f;
-    private const string HorizontalAxis = "Horizontal";
-    [SerializeField] private float _speedX = 0.5f;
+    private const float speedBooster = 50f;
+    private const string horizontalAxis = "Horizontal";
+    private const string groundTag = "Ground";
+
+    [SerializeField] private float _speedX = 1f;
+    [SerializeField] private float _jumpForce = 500f;
+
     private Rigidbody2D _rigidbody;
     private float direction;
+    private bool _isJump;
+    private bool _isGround;
 
     void Start()
     {
@@ -17,12 +23,28 @@ public class PlayerMover : MonoBehaviour
     }
     private void Update()
     {
-        direction = Input.GetAxis(HorizontalAxis);
-        Debug.Log(direction);
+        direction = Input.GetAxis(horizontalAxis);
+        if (_isGround && Input.GetKeyDown(KeyCode.W))
+        {
+            _isJump = true;
+        }
     }
     private void FixedUpdate()
     {
         _rigidbody.velocity = new Vector2(_speedX * direction * speedBooster * Time.fixedDeltaTime, _rigidbody.velocity.y);
-        
+
+        if (_isJump)
+        {
+            _rigidbody.AddForce(new Vector2(0, _jumpForce));
+            _isJump = false;
+            _isGround = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(groundTag))
+        {
+            _isGround = true;
+        }
     }
 }
